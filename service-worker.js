137532ddfdcce71e9e3a54caed1e142e9fe8c15a
luminous66666 <luminous66666666@outlook.com>
2024-12-12 08:34:30 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "animations-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./animations.css",
+  "./icons/luminous.jpeg",
+  "./animations/the_cow.mp4",
+  "./animations/the_embrace.mp4"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
